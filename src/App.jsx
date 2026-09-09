c8f07220056692610ca12a8963f44860903b3f55
RobinJohansson1992 <robin-johansson@live.com>
2026-09-09 09:53:53 +0200

    added router to pages and it seems to be working as it should

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import AllCards from "./views/AllCards";
+import { RouterProvider } from "react-router-dom";
+
+import Router from "./router/Router";
 
 function App() {
   return (
     <main>
       <div className="mainContainer">
-        <AllCards />
+        {/* <AllCards /> */}
+        <RouterProvider router={Router} />
       </div>
     </main>
   );
---
+++ src/views/AddCard.jsx
@@ -0,0 +1,8 @@
+function AddCard() {
+  return (
+    <>
+      <h1>ADD CARD</h1>
+    </>
+  );
+}
+export default AddCard;
