d5677688b4d53cfdcb667c6ca8281bbfa891a422
RobinJohansson1992 <robin-johansson@live.com>
2026-09-09 10:38:03 +0200

    Added html and css for the form

--- src/views/AddCard.jsx
+++ src/views/AddCard.jsx
@@ -8,6 +8,28 @@ function AddCard() {
     <>
       <Header header="ADD CARD" activeCard="NEW CARD" />
       <Card />
+      <section className="cardForm">
+        <p>CARD NUMBER</p>
+        <input type="text" className="bigInput" />
+        <p>CARDHOLDER NAME</p>
+        <input
+          placeholder="FIRSTNAME LASTNAME"
+          type="text"
+          className="bigInput"
+        />
+        <div className="smallFields">
+          <div className="smallFieldContainer">
+            <p>VALID THRU</p>
+            <input type="date" className="smallInput" />
+          </div>
+          <div className="smallFieldContainer">
+            <p>CCV</p>
+            <input type="text" className="smallInput" />
+          </div>
+        </div>
+        <p>VENDOR</p>
+        <select name="" id="" className="bigInput"></select>
+      </section>
       <Link to="/" className="navBtn">
         ADD CARD
       </Link>
